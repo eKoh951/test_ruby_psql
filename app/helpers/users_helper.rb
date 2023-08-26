509463d9c6c870d9b242339d5588eb2333e34259
eKoh951 <greatekoh@gmail.com>
2023-08-26 16:51:44 -0600

    Added env
    Playing a bit with ruby

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,2 +1,15 @@
 module UsersHelper
+	class Person
+		attr_reader :first_name, :last_name, :email
+
+		def initialize(first_name, last_name, email)
+			@first_name = first_name
+			@last_name = last_name
+			@email = email
+		end
+
+		def present
+			"Hello, I'm #{@first_name} #{@last_name} and my email is #{@email}"
+		end
+	end
 end
